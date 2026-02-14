ec8854f176e3ce194dc9655030ff984fdca91fe3
Deepak <appudeepak944@gmail.com>
2026-02-14 15:50:55 +0530

    use data from the backend to jenerate the html

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,17 @@
 import './HomePage.css'
 import { Header } from '../components/Header';
-import { products } from '../../starting-code/data/products'; 
 import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 export default function HomePage() {
-  axios.get('http://localhost:3000/api/products')
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+     axios.get('http://localhost:3000/api/products')
     .then((response) => {
-      console.log(response.data);
+      setProducts(response.data);
     });
+  },[]);
+  
   return (
     <>
       <Header />
