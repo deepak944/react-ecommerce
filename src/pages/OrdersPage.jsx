99392f0b7c0aad16896950757f8a88fe48246ffc
Deepak <appudeepak944@gmail.com>
2026-02-16 10:37:00 +0530

    generate the html for orderpage

--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -1,48 +1,66 @@
 import './OrdersPage.css'
 import { Header } from '../components/Header';
-export function OrdersPage() {
+import axios from 'axios';
+import { useState, useEffect,Fragment} from 'react';
+import dayjs from 'dayjs';
+import { formatMoney } from '../utils/money';
+export function OrdersPage({ cart }) {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/orders?expand=products')
+      .then((response) => {
+        setOrders(response.data);
+      });
+  }, []);
+
   return (
     <>
-      <Header />
+      <Header cart={cart} />
 
     <div className="orders-page">
       <div className="page-title">Your Orders</div>
 
       <div className="orders-grid">
-        <div className="order-container">
+        {orders.map((order) => {
+          return ( 
+           <div key={order.id} className="order-container">
 
           <div className="order-header">
             <div className="order-header-left-section">
               <div className="order-date">
                 <div className="order-header-label">Order Placed:</div>
-                <div>August 12</div>
+                <div>{dayjs(order.orderTimesMs).format('MMMM D, YYYY')}</div>
               </div>
               <div className="order-total">
                 <div className="order-header-label">Total:</div>
-                <div>$35.06</div>
+                <div>${formatMoney(order.totalCostCents)}</div>
               </div>
             </div>
 
             <div className="order-header-right-section">
               <div className="order-header-label">Order ID:</div>
-              <div>27cba69d-4c3d-4098-b42d-ac7fa62b7664</div>
+              <div>{order.id}</div>
             </div>
           </div>
 
           <div className="order-details-grid">
-            <div className="product-image-container">
-              <img src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+            {order.products.map((orderProduct) => {
+              return (
+                <Fragment key={orderProduct.product.id}>
+                 <div className="product-image-container">
+              <img src={orderProduct.product.image} />
             </div>
 
             <div className="product-details">
               <div className="product-name">
-                Black and Gray Athletic Cotton Socks - 6 Pairs
+                {orderProduct.product.name}
               </div>
               <div className="product-delivery-date">
-                Arriving on: August 15
+                Arriving on:{dayjs(orderProduct.estimatedDeliveryTimeMs).format('MMMM D, YYYY')}
               </div>
               <div className="product-quantity">
-                Quantity: 1
+                Quantity: {orderProduct.quantity}
               </div>
               <button className="buy-again-button button-primary">
                 <img className="buy-again-icon" src="images/icons/buy-again.png" />
@@ -57,26 +75,10 @@ export function OrdersPage() {
                 </button>
               </a>
             </div>
-
-            <div className="product-image-container">
-              <img src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg" />
-            </div>
-
-            <div className="product-details">
-              <div className="product-name">
-                Adults Plain Cotton T-Shirt - 2 Pack
-              </div>
-              <div className="product-delivery-date">
-                Arriving on: August 19
-              </div>
-              <div className="product-quantity">
-                Quantity: 2
-              </div>
-              <button className="buy-again-button button-primary">
-                <img className="buy-again-icon" src="images/icons/buy-again.png" />
-                <span className="buy-again-message">Add to Cart</span>
-              </button>
-            </div>
+                </Fragment>
+              );})}
+            
+    
 
             <div className="product-actions">
               <a href="/tracking">
@@ -87,6 +89,9 @@ export function OrdersPage() {
             </div>
           </div>
         </div>
+           );
+           })}
+        
 
         <div className="order-container">
 
